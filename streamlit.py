a2d4562cbf6f7c8cd9100226b624e2f36dd499e0
iandresh <115046768+iandresh@users.noreply.github.com>
2023-04-03 21:07:49 -0400

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,19 @@
 import streamlit as st
 
 def main():
-    st.title("Boot Camp Certificate Minter")
-    st.write("Choose an account to get started")
+    st.title("Bootcamp Certificate Minter")
+st.write("Choose an account to get started")
+accounts = 0xfFa2994F451c0342aF46e055DAC924b8fe302285
+address = st.selectbox("Select Account", options=accounts)
+st.markdown("---")
+
+# ADD CERTIFICATE
+st.markdown("## Mint Bootcamp Certificate")
+
+tab1 = st.tabs(["Individual"])
+
+# INDIVIDUAL
+with tab1:
     name = st.text_input("Full Name")
     completion_date = st.text_input("Completion Date")
     certificate_template = st.file_uploader("Upload Certificate Template")
